2eb9e3cd63f05e4453e494d01af444e6f1b5b137
Nitin Goley <113915570+nitingoley@users.noreply.github.com>
2025-01-13 12:34:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://movie-full-stack-web-app.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
