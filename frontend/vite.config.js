b2da84bbd960a765b56cd450a3588b3f0f84f40d
Nitin Goley <113915570+nitingoley@users.noreply.github.com>
2025-01-13 12:34:13 +0530

    Add onrender live url vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:3000",
-      "/uploads": "http://localhost:3000",
+      "/api/": "https://movie-full-stack-web-app-2.onrender.com",
+      "/uploads": "https://movie-full-stack-web-app-2.onrender.com",
     },
   },
 });
