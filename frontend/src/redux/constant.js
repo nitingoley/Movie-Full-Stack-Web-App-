b2d7229dbfff0f37e8c4e1bcc3e39c82edbe185e
nitin <nitingoley21@gmail.com>
2025-01-31 23:20:19 +0530

    proxy error fix

--- frontend/src/redux/constant.js
+++ frontend/src/redux/constant.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "";
-export const USERS_URL= "/api/v1/users";
-export const GENRE_URL= "/api/v1/genre"; 
-export const MOVIE_URL = "/api/v1/movies";
-export const UPLOAD_URL = "/api/v1/upload";
+export const BASE_URL = "https://movie-full-stack-backend.onrender.com";  
+export const USERS_URL = `${BASE_URL}/api/v1/users`;
+export const GENRE_URL = `${BASE_URL}/api/v1/genre`;
+export const MOVIE_URL = `${BASE_URL}/api/v1/movies`;
+export const UPLOAD_URL = `${BASE_URL}/api/v1/upload`;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/": "https://movie-full-stack-backend.onrender.com",
-      "/uploads": "https://movie-full-stack-backend.onrender.com",
-    },
-  },
 });
