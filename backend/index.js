0d2e710fa32eb5a8640bd15cc7fd962b004d0417
Nitin Goley <113915570+nitingoley@users.noreply.github.com>
2025-02-21 18:45:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 // CORS Configuration
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://moviefx.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
